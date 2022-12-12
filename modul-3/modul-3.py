060bd9e045c3cbcc79b76b612510c99c10b4cc14
Ekaterina Chudova <testerchudova@gmail.com>
2022-12-12 20:30:22 +0300

    Реализована тестовая возможность запуска браузера

--- modul-3/modul-3.py
+++ modul-3/modul-3.py
@@ -1,3 +1,16 @@
 import math
+import  numpy as np
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options as Firefox_options
+path = "..\..\geckodriver-v0.31.0-win64\geckodriver.exe"
 
-print(f"аааааа {math.pi} ")
+def run_script():
+    options = Firefox_options()
+    driver = Firefox(executable_path=path, options=options)
+    driver.get("https://go.skillbox.ru/")
+    #driver.quit()
+
+
+
+if __name__ == "__main__":
+    run_script()
