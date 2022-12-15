190035504655ee20cb735ddb30ecf66753e8d797
Ekaterina Chudova <testerchudova@gmail.com>
2022-12-15 20:28:06 +0300

    commit message

--- modul-3/modul-3.py
+++ modul-3/modul-3.py
@@ -1,3 +1,19 @@
-import math
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options as Chrome_Options
+import chromedriver_binary
 
-print(f"аааааа {math.pi} ")
+print("Start")
+
+def run_script():
+    options = Chrome_Options()
+    driver = Chrome(options=options)
+    driver.get("https://github.com/testerchudova/autotests-in-python")
+    driver.get("https://git-scm.com/download/win")
+    #driver.quit()
+
+#run_script()
+options = Chrome_Options()
+driver = Chrome(options=options)
+driver.get("https://github.com/testerchudova/autotests-in-python")
+driver.get("https://git-scm.com/download/win")
+driver.get_screenshot_as_file(r"c:\Users\git")
