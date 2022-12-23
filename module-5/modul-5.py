c3c9d21bae22e9e0f2c8bd591e620ad681b9e465
Ekaterina Chudova <testerchudova@gmail.com>
2022-12-23 05:17:59 +0300

    Сделаны 2, 3, 4 Кейсы

--- module-5/modul-5.py
+++ module-5/modul-5.py
@@ -1,4 +1,5 @@
 from time import sleep
+from random import uniform
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -11,12 +12,11 @@ from pathlib import Path
 current_path = Path(__file__)
 driver_path = current_path.parent.parent.joinpath("geckodriver-v0.31.0-win64", "geckodriver.exe")
 
-path = "..\.\geckodriver-v0.31.0-win64\geckodriver.exe"
 
 class Tester():
     options = Firefox_options()
     options.page_load_strategy = "normal"
-    driver = Firefox(executable_path=path, options=options)
+    driver = Firefox(executable_path=driver_path, options=options)
     
     @classmethod
     def is_element(cls, common_by, selector):
@@ -83,7 +83,7 @@ class Tester():
         input_1 = Tester.driver.find_element(By.XPATH, '//input[@id="author-filter-field"]')
         for simbol in input_text:
             input_1.send_keys(simbol)
-            ActionChains(Tester.driver).pause(0.2).perform()
+            ActionChains(Tester.driver).pause(uniform(0.2, 6)).perform()
             
 
 
@@ -91,16 +91,14 @@ class Tester():
             .until(lambda d: d.find_element(By.XPATH, '//button[@value="bpasero"]'))
         item_list.click()
 
-        value_input = Tester.driver.find_element(By.CSS_SELECTOR, '#js-issues-search').get_attribute("value")
+        is_text__value = text_to_be_present_in_element_value((By.CSS_SELECTOR, '#js-issues-search'), input_text)(Tester.driver)
+        assert is_text__value == True, f"В строке поиска отсутствует проверяемая сторка {input_text}"
 
-        text_to_be_present_in_element_value((By.CSS_SELECTOR, '#js-issues-search'), input_text)
-
+        print("Тест завершен успешно")
 
 
         pass
 
-        #ActionChains(Tester.driver).pause(1).perform()
-        #Tester.driver.find_element(By.XPATH, '//button[@value="bpasero"]').click()
     def test_filling_out_form(seif):
         """
         Кейс №3
@@ -110,18 +108,44 @@ class Tester():
         3 В поле количества звёзд у репозитория выберите >20000.
         4 В поле с названием файла выберите environment.yml.
         5 Нажмите на кнопку поиска.
-        6 Остановите выполнение автотеста и глазами проверьте, что отображаются репозитории по выбранным критериям.  
+        6 Соберите информацию по всем репозиториям
+        7 Проверьте, что в списке отображаются репозитории с количеством звёзд >20000
         """
-        
+        namber = 20000
         page = Tester.driver.get("https://github.com/search/advanced")
         find_el_select_language = Tester.driver.find_element(By.XPATH, '//select[@id="search_language"]')
         find_el_select_language.find_element(By.XPATH, '//option[@value="Python"]').click()
         
 
-        Tester.driver.find_element(By.XPATH, '//input[@id="search_stars"]').send_keys(">20000")
+        Tester.driver.find_element(By.XPATH, '//input[@id="search_stars"]').send_keys(f">{namber}")
         Tester.driver.find_element(By.XPATH, '//input[@id="search_filename"]').send_keys("environment.yml")       
         
         Tester.driver.find_element(By.XPATH, '//div[@class="form-group flattened"]//button').click()
+
+        
+        
+        page = 1
+        namber = namber/1000        
+        
+        while not Tester.is_element(By.CSS_SELECTOR, 'span[class="next_page disabled"]'):
+            try:                
+                items_list = WebDriverWait(Tester.driver, timeout=6) \
+                    .until(lambda d: d.find_elements(By.XPATH, '//a[@class = "Link--muted"]'))
+                test_page_ok = all([float(item.text[0:-1]) > namber for item in items_list])                
+                assert test_page_ok == True, f"Количество звезд не соответствует условию > {namber}k"                
+                Tester.driver.find_element(By.CSS_SELECTOR, 'a.next_page').click()
+                ActionChains(Tester.driver).pause(uniform(4, 10)).perform()
+                
+            except StaleElementReferenceException:
+                continue
+            page += 1
+        
+        print("Тест завершен успешно.")
+
+
+        pass
+    
+    
    
    
    
@@ -133,7 +157,7 @@ class Tester():
         2 Выберите радио-баттон с названием «Профессия» в разделе «Тип обучения на платформе».
         3 Укажите в поле «Длительность» диапазон от 6 до 12 месяцев (через движение мышки).
         4 В тематике выберите любой из чекбоксов.
-        5 Остановите выполнение автотеста и глазами проверьте, что отображаются правильные данные. 
+        5 Проверьте, что в списке находятся те курсы, которые вы ожидали. 
         """
         
         page = Tester.driver.get("https://skillbox.ru/code/")
@@ -154,6 +178,43 @@ class Tester():
             .move_by_offset(-60, 0)\
             .release()\
             .perform()
+
+        list_courses = Tester.driver.find_elements(By.CSS_SELECTOR, 'a.ui-product-card-main__wrap')
+        second = 6
+        if Tester.is_element(By.CSS_SELECTOR,'button.courses-block__load'):
+            ActionChains(Tester.driver).pause(1).perform()
+            Tester.driver.find_element(By.CSS_SELECTOR, 'button.courses-block__load').click() 
+
+            for time_1 in range(1, second*2 + 1):
+                list_courses_2 = Tester.driver.find_elements(By.CSS_SELECTOR, 'a.ui-product-card-main__wrap')
+                
+                if len(list_courses_2) == len(list_courses):
+                    ActionChains(Tester.driver).pause(.5).perform()
+                else:
+                    list_courses = list_courses_2
+                    break
+
+                if time_1 == second*2:
+                    raise "Время истекло"
+
+
+
+        list_cour = ['разработ', 'android', 'developer']    
+        res = []
+
+        for item_courses in list_courses:
+            for item_cour in list_cour:                    
+                if item_cour in item_courses.text.lower():
+                    res_loc = True
+                    break
+                res_loc = False                
+            res += [res_loc]
+
+        assert all(res) == True, f"Не все во всех карточках содержится хотябы одно из слов {list_cour}"
+
+        print("Тест завершен успешно.")
+
+        pass
 
     def test_hover(seif):
         """
@@ -181,8 +242,8 @@ if __name__ == "__main__":
     
     tester = Tester()
     # tester.test_find_title_bug()
-    tester.test_select_from_list()
-    # tester.test_filling_out_form()
+    # tester.test_select_from_list()
+    #tester.test_filling_out_form()
     #tester.test_course_selection()
-    #tester.test_hover()
+    tester.test_hover()
 
