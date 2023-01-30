0ccb9c9bea118ceeb841f4346f4402ebd1e0288c
Ekaterina Chudova <testerchudova@gmail.com>
2023-01-30 16:09:10 +0300

    save

--- final_work/conftest.py
+++ final_work/conftest.py
@@ -1,15 +1,16 @@
 import logging.config
 from pathlib import Path
+
 current_path = Path(__file__)
 logging_path = current_path.parent.joinpath("logging.ini")
 logging.config.fileConfig(logging_path)
-
 
 
 def pytest_addoption(parser):
     parser.addini("nameuser", "Имя пользователя")
     parser.addini("user_email", "Email пользователя")
     parser.addini("password_user", "Пароль пользователя")
+
 
 pytest_plugins = [
     "src.fixtures"
